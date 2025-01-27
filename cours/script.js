8157d0844cb59e941405a756ef09f55a41dfe15e
styvo123 <arnaudnguenang0@gmail.com>
2025-01-27 16:01:40 +0100

    lecon3

---
+++ cours/script.js
@@ -0,0 +1,29 @@
+let nomber = 2
+console.log(typeof(Number));
+
+// les tableaux
+
+let array2 = ["Bafou","Batie","Bafang","Bamenda"];
+console.log(array2[0][2]);
+
+let objet = {
+    firstname: "rosto",
+    lastname: "dev",
+    age: 29,
+    technos: ["Javascript", "NodeJs", "React", "laravel", "Flutter"],
+    admin: false
+    }
+    console.log(typeof(objet));
+    console.log(objet.technos[3]);
+
+    objet.adress="Bafoussam";
+    console.log(objet);
+
+    objet.adress="Bafan";
+    console.log(objet);
+
+    objet.nomber=22
+    console.log(objet);
+    
+    
+    
